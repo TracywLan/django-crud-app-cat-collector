af136a28fc6fa82d37c6506d71bf54f274f920e9
TracywLan <tracylan12@gmail.com>
2026-02-20 17:40:20 -0500

    finishes update button

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Import HttpResponse to send text-based responses
@@ -27,6 +27,15 @@ from .models import Cat
 class CatCreate(CreateView):
     model = Cat
     fields = '__all__'
+    
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
 
 # Define the home view function
 def home(request):
@@ -42,7 +51,7 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cats': cat})
+    return render(request, 'cats/detail.html', {'cat': cat})
 
 
 
